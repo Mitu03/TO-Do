049e6a3d94f634b3aa5e47bc44f8dadfae2e0e62
Mitu kr Rabha <166889598+Mitu03@users.noreply.github.com>
2025-01-14 18:37:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import { QuizProvider } from "./contexts/QuizContext";
 
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-
-function addTask(){
-    if(inputBox.value === ' '){
-       alert("You must write something");
-    }else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
-}
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
-
-function saveData(){
-    localStorage.setItem("data" , listContainer.innerHTML);
-}
-
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <QuizProvider>
+      <App />
+    </QuizProvider>
+  </React.StrictMode>
+);
